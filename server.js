e9ebb947da18bf92eb350a269cfbaaa3077b826a
FelipeAugusto <94065571+FelipeAugustoSilva@users.noreply.github.com>
2024-06-19 02:55:10 -0300

    Atualizando index

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//node server.js
-
 const http = require('http');
 const host = "http://localhost";
 const port = 3000;
@@ -7,12 +5,13 @@ const port = 3000;
 const stats = require('./pcRam.js');
 
 http.createServer((req, res) => {
-    let url = req.url;
+  let url = req.url;
 
-    if (url === '/stats') {
-     res.end(JSON.stringify(stats, null, 2));
-    } else {
-        res.end('<h1>Seja bem vindo</h1>')
-    }
+  if (url === '/status') {
+    res.end(JSON.stringify(stats, null, 2));
+  } else {
+    res.end('<h1>Seja bem-vindo <a href="http://localhost:3000/status">http://localhost:3000/status</a></h1>');
+  }
 }).listen(port, () => console.log(`Rodando no ${host}:${port}`));
+
 
